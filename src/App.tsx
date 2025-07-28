7720dda5f92e552ff90d5744125a1afd557b7591
evepanzarino <109627880+evepanzarino@users.noreply.github.com>
2025-07-27 20:38:40 -0400

    header react js

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,84 @@ import './App.css'
 function App() {
   return (
     <>
-    <div  className="header-grid">
-	<div  className="header-item">
-	<a href="https://panzarino.org"><h3 style="margin-top:-5px;text-align:center;font-size: 25px; font-weight:700; font-family: 'Alegreya';"><i class="fa fa-home" aria-hidden="true"><p class="home-header">Home</p></i></h3></a>
-	</div>
-	<div  className="header-item">
-	<a href="https://panzarino.org/shop"><h3 style="margin-top:-5px;text-align:center;font-size: 25px; font-weight:700; font-family: 'Alegreya';"><i class="fa-solid fa-bag-shopping"><p class="shop">Services</p></i></h3></a>
-	</div>
-	<div  className="header-item">
-		<a href="https://panzarino.org"><h3 style="font-size: 24px;font-weight:700; font-family: 'Alegreya';">Panzarino</h3></a>
-	
-	</div>
-	<div  className="header-item">
-	<a href="https://panzarino.org/posts"><h3 style="margin-top:-5px;text-align:center;font-size: 25px; font-weight:700; font-family: 'Alegreya';"><i class='fas fa-blog'><p class="blog">Blog</p></i></h3></a>
-	</div>
-		<div className="header-item">
-	<a href="https://panzarino.org/checkout"><h3 style="margin-top:-5px;text-align:center;font-size: 25px; font-weight:700; font-family: 'Alegreya';"><i class="fa-solid fa-cart-shopping"><p class="cart">Cart</p></i></h3></a>
-	</div>
-	</div>
+ <div className="header-grid">
+  <div className="header-item">
+    <a href="https://panzarino.org">
+      <h3
+        style={{
+          marginTop: "-5px",
+          textAlign: "center",
+          fontSize: 25,
+          fontWeight: 700,
+          fontFamily: '"Alegreya"'
+        }}
+      >
+        <i className="fa fa-home" aria-hidden="true">
+          <p className="home-header">Home</p>
+        </i>
+      </h3>
+    </a>
+  </div>
+  <div className="header-item">
+    <a href="https://panzarino.org/shop">
+      <h3
+        style={{
+          marginTop: "-5px",
+          textAlign: "center",
+          fontSize: 25,
+          fontWeight: 700,
+          fontFamily: '"Alegreya"'
+        }}
+      >
+        <i className="fa-solid fa-bag-shopping">
+          <p className="shop">Services</p>
+        </i>
+      </h3>
+    </a>
+  </div>
+  <div className="header-item">
+    <a href="https://panzarino.org">
+      <h3 style={{ fontSize: 24, fontWeight: 700, fontFamily: '"Alegreya"' }}>
+        Panzarino
+      </h3>
+    </a>
+  </div>
+  <div className="header-item">
+    <a href="https://panzarino.org/posts">
+      <h3
+        style={{
+          marginTop: "-5px",
+          textAlign: "center",
+          fontSize: 25,
+          fontWeight: 700,
+          fontFamily: '"Alegreya"'
+        }}
+      >
+        <i className="fas fa-blog">
+          <p className="blog">Blog</p>
+        </i>
+      </h3>
+    </a>
+  </div>
+  <div className="header-item">
+    <a href="https://panzarino.org/checkout">
+      <h3
+        style={{
+          marginTop: "-5px",
+          textAlign: "center",
+          fontSize: 25,
+          fontWeight: 700,
+          fontFamily: '"Alegreya"'
+        }}
+      >
+        <i className="fa-solid fa-cart-shopping">
+          <p className="cart">Cart</p>
+        </i>
+      </h3>
+    </a>
+  </div>
+</div>
+
     </>
   )
 }
